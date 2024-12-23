d7944c6e814bbd3e006d2c39f226294a3f16ecb1
Arthur Gabriel Lima Gomes <78980894+ArthurGabrieel@users.noreply.github.com>
2024-12-23 20:06:08 -0300

    new solution for divide and conquer #4
    Co-Authored-By: Alexandre Beck <82113486+zzzbeck@users.noreply.github.com>

--- dc-4.ts
+++ dc-4.ts
@@ -1,22 +1,43 @@
-function findMedianSortedArraysBroken(
+function findMedianSortedArraysAlmost(
   nums1: number[],
   nums2: number[]
 ): number {
-  // Junta ambos os arrays (custo O(m + n))...
-  const merged = nums1.concat(nums2);
-  // ... mas ordena de maneira que o custo seja O((m+n) * log(m+n)) - o que fura o requisito O(log(m+n))
-  merged.sort((a, b) => a - b);
-
-  const len = merged.length;
-  // Pega o valor do meio
-  if (len === 0) return 0; // falha feia em caso de arrays vazios
-
-  if (len % 2 === 1) {
-    // Mediana para tamanho ímpar
-    return merged[Math.floor(len / 2)];
-  } else {
-    // Mediana para tamanho par
-    const mid = len / 2;
-    return (merged[mid - 1] + merged[mid]) / 2;
+  // Tentar garantir que nums1 seja sempre o menor array:
+  if (nums1.length > nums2.length) {
+    return findMedianSortedArraysAlmost(nums2, nums1);
   }
+
+  const m = nums1.length;
+  const n = nums2.length;
+
+  let left = 0;
+  let right = m;
+
+  while (left <= right) {
+    // Faz a partição em nums1
+    const i = Math.floor((left + right) / 2);
+    // Tenta calcular a partição em nums2
+    const j = Math.floor((m + n) / 2) - i;
+
+    const left1 = nums1[i - 1];
+    const right1 = nums1[i];
+    const left2 = nums2[j - 1];
+    const right2 = nums2[j];
+
+    if (left1 <= right2 && left2 <= right1) {
+      if ((m + n) % 2 === 0) {
+        return (Math.max(left1, left2) + Math.min(right1, right2)) / 2;
+      } else {
+        return Math.max(left1, left2);
+      }
+    } else if (left1 > right2) {
+      // Mover partição para a esquerda
+      right = i - 1;
+    } else {
+      // Mover partição para a direita
+      left = i + 1;
+    }
+  }
+
+  return -1;
 }
